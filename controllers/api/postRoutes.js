ac46b1d14e2120fe234fe948cdf90d9b706399b9
gwennr-t <gwennt321@gmail.com>
2024-04-28 22:18:38 -0500

    Updated postRoutes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,37 +2,43 @@ const router = require('express').Router();
 const { User,Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
-    try {
-// post findAll
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// router.get('/', (req, res) => {
+//     try {
+// // post findAll
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
-router.get('/:id', (req, res) => {
-    try {
-// post findOne
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// router.get('/:id', (req, res) => {
+//     try {
+// // post findOne
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 router.post('/', withAuth, (req, res) => {
     try {
-// post create
+    
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newPost);
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-router.put('/:id', withAuth,(req, res) => {
-    try {
-// post update
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// router.put('/:id', withAuth,(req, res) => {
+//     try {
+// // post update
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 router.delete('/:id', withAuth,(req, res) => {
     try {
